34a355f00932566b73773febde4419bef90f7285
jhassan <jawadjee0519@gmail.com>
2016-04-21 00:48:14 +0500

    update code

--- action.php
+++ action.php
@@ -13,22 +13,25 @@
 		// Get all shoes for sale
 		case "GetAllShoes":
 			$keyword = '%'.$_POST['keyword'].'%';
-
-			$query = "SELECT `article_no`, `sh_credit`, `date_purchase`, `size_code`, SUM(`sh_credit`) AS credit_shoes, 
+			if(!empty($keyword) && $keyword != "%%")
+			{
+				$query = "SELECT `article_no`, `sh_credit`, purchase_detail_id, `date_purchase`, `size_code`, SUM(`sh_credit`) AS credit_shoes, 
 					`tbl_purchase_detail`.`purchase_id`, MAX(`sale_price`) AS shoes_price
 					FROM `tbl_purchase`
 					INNER JOIN `tbl_purchase_detail` ON `tbl_purchase_detail`.`purchase_id` = `tbl_purchase`.`purchase_id`
 					WHERE `article_no` LIKE '".$keyword."'
 					GROUP BY `size_code`";
 					// echo $query; die;
-			$list = MySQLQuery($query);
-			 // pr($list); die;	
-			foreach ($list as $rs) {
-			// put in bold the written text
-			$article_no = str_replace($_POST['keyword'], '<b>'.$_POST['keyword'].'</b>', $rs['article_no']);
-			// add new option
-		    // echo '<li onclick="set_item(\''.str_replace("'", "\'", $rs['article_no']).'\'); clickme('.$rs['purchase_detail_id'].');" style="cursor: pointer;">'.$article_no.'</li>';
-		}echo '<li onclick="AddProductToSale('$rs['purchase_id']),$rs['article_no'],$rs['purchase_detail_id']'); " style="cursor: pointer;">'.$article_no.'</li>';
+				$list = MySQLQuery($query);
+				 // pr($list); die;	
+				foreach ($list as $rs) {
+				// put in bold the written text
+				$article_no = str_replace($_POST['keyword'], '<b>'.$_POST['keyword'].'</b>', $rs['article_no'])."-".$rs['size_code'];
+				// add new option
+			     echo '<li onclick="AddProductToSale('.$rs['purchase_detail_id'].','.$rs['article_no'].','.$rs['shoes_price'].',\''.$rs['size_code'].'\'); set_item(\''.str_replace("'", "\'", $rs['article_no']).'\')"; style="cursor: pointer;">'.$article_no.'</li>';
+			}	
+			
+		} //echo '<li onclick="AddProductToSale('$rs['purchase_id'],$rs['article_no'],$rs['shoes_price']'); " style="cursor: pointer;">'.$article_no.'</li>';
 		break;
 		
 		
--- sale.php
+++ sale.php
@@ -2,7 +2,7 @@
 
          <?php include_once('header.php');?>
     <!-- Navigation -->
-    <div class="row "> 
+    <div class=""> 
         <div class="bs-example" data-example-id="inverted-navbar">
             <nav class="navbar navbar-inverse">
               <div class="container">
@@ -47,27 +47,25 @@
         <div class="col-md-8 noprint" style="overflow: auto; max-height: 500px;">
         <div class="col-md-6">
             <label>Select any shoes!</label>
-            <input type="text" id="search_id" class='form-control' onkeyup="autocomplet()">
+            <input type="text" id="search_id" class='form-control number_only' onclick="EmptyTextField();" onkeyup="autocomplet()">
             <ul id="article_list_id" ></ul>
         </div>        
         </div>
         
         <div class="col-md-4" id="InvoiceDiv">          
-            <div class="text-center"><img height="35" src="http://sales.pakcappellos.com/img/logo1.png" alt="Cappellos" /></div>
           <div class="bs-example" data-example-id="simple-table"> 
           	<form class="form-signin" id="FormID" method="post" onsubmit="return false;">
-        		<input type="hidden" name="_token" value="AV1SzkvuV6InJs1izXms9rsQ8P85odNu9mTxTc3A" />
             <table class="table table table-bordered" width="100%" style="table-layout:fixed; margin-bottom:0px; font-size:12px;"> 
             <tbody class="border"> 
                 <tr> 
-                	<td colspan="2" style="overflow: hidden;" class="text-center">Fried Roll Ice Cream</td> 
+                	<td colspan="2" style="overflow: hidden; font-weight: bold; font-size: 18px;" class="text-center">Millon Shoes</td> 
                  </tr>
                 <tr> 
-                    <td colspan="2" style="overflow: hidden;" class="text-center">3rd Floor Unitd Mall Multan</td>
+                    <td colspan="2" style="overflow: hidden;" class="text-center">Tahir Plaza Dahli Gate Multan</td>
                 </tr>
                 <tr> 
-                    <td width="45%" class="col-md-6">Invoice#: MUL-47</td> 
-                    <td width="55%" class="col-md-6">Date:13-Apr-2016</td> 
+                    <td width="45%" class="col-md-6">Invoice#: 0</td> 
+                    <td width="55%" class="col-md-6">Date:<?php echo date("d-M-Y")?></td> 
                 </tr> 
               </tbody>
             </table>
@@ -108,7 +106,7 @@
                             <td class="col-md-12" colspan="3">Thanks for choosing Cappellos</td> 
                         </tr>
                         <tr> 
-                            <td class="col-md-12" colspan="3">Developed by: (0334)6026706, (0321)6328470</td> 
+                            <td class="col-md-12" colspan="3">By: (0334)6026706, (0321)6328470</td> 
                         </tr> 
                         <tr class="noprint"> 
                             <td class="col-md-12" colspan="3" align="right"><button type="button" onclick="return CheckValidate();" class="btn btn-success">Save and Print</button></td> 
@@ -152,10 +150,218 @@ function set_item(item) {
     $('#article_list_id').hide();
 }
 
+function EmptyTextField()
+{
+    $('#search_id').val('');
+}
 // function clickme(id)
 // {
 //     alert('I am here1'+id);
 // }
+</script>
+<script type="text/javascript">
+        $(document).ready(function(e) {
+     $('#FormID')[0].reset();
+     $('#btnSave').prop('disabled', false);
+     $('#PaidAmount').prop('disabled', false);
+          
+          // Numeric only control handler
+    jQuery.fn.ForceNumericOnly =
+    function()
+    {
+      return this.each(function()
+      {
+        $(this).keydown(function(e)
+        {
+          var key = e.charCode || e.keyCode || 0;
+          // allow backspace, tab, delete, enter, arrows, numbers and keypad numbers ONLY
+          // home, end, period, and numpad decimal
+          return (
+            key == 8 || 
+            key == 9 ||
+            key == 13 ||
+            key == 46 ||
+            key == 110 ||
+            key == 190 ||
+            (key >= 35 && key <= 40) ||
+            (key >= 48 && key <= 57) ||
+            (key >= 96 && key <= 105));
+        });
+      });
+    };   
+    
+    // Call Only numbers
+    $(".number_only").ForceNumericOnly();
+    
+    // Keypress add commas in numbers
+     $('input.number_only').keyup(function(event){
+        // skip for arrow keys
+        if(event.which >= 37 && event.which <= 40){
+          event.preventDefault();
+        }
+        var $this = $(this);
+        var num = $this.val().replace(/,/gi, "").split("").reverse().join("");
+        var num2 = RemoveRougeChar(num.replace(/(.{3})/g,"$1,").split("").reverse().join(""));
+        // the following line has been simplified. Revision history contains original.
+        $this.val(num2);
+      });
+        
+        $('input.number_only')
+        .on('focus', function(){
+                var $this = $(this);
+                if($this.val() == 0){
+                        $this.val('');
+                }
+        })
+        .on('blur', function(){
+                var $this = $(this);
+                if($this.val() == ''){
+                        $this.val(0);
+                }
+        })
+    
+            
+          
+}); // End ready
+    function printDiv() {    
+    var printContents = document.getElementById('InvoiceDiv').innerHTML;
+    var originalContents = document.body.innerHTML;
+     document.body.innerHTML = printContents;
+     window.print();
+     document.body.innerHTML = originalContents;
+    }
+        
+        // AddProductToSale()
+        function AddProductToSale(id,article_no,shoes_price,size_code)
+        {
+          
+          var AlreadyId = $("#Product_"+id+"").closest('tr').attr('id');
+          if(AlreadyId)
+          {
+          // Net Amount
+          var CurrentValue = $("#NetAmount").val();
+          var NetAmount = parseInt(shoes_price) + parseInt(CurrentValue);
+          $("#NetAmount").val(NetAmount);
+          //$("#net_amount").val(NetAmount);
+          // Quantity
+          var Qty = $("#Qty_"+id+"").html();
+          var NetQty = parseInt(1) + parseInt(Qty);
+          $("#Qty_"+id+"").html(NetQty);
+          $("#TotalQty_"+id+"").val(NetQty);
+          
+          // Price
+          var ProductPrice = $("#ProductPrice_"+id+"").val();
+          var NetProductPrice = parseInt(shoes_price) + parseInt(ProductPrice);
+          $("#ProductPrice_"+id+"").val(NetProductPrice);
+          $("#TotalProductPrice_"+id+"").html(NetProductPrice);
+          }
+          else
+          {
+          var CurrentValue = $("#NetAmount").val();
+          var NetAmount = parseInt(shoes_price) + parseInt(CurrentValue);
+          $("#NetAmount").val(NetAmount);
+          var str = "";
+          //size_code = 36;
+          str = "<tr class='count_product' id='Product_"+id+"' onclick='DeleteProduct("+id+","+shoes_price+");' class='cursor'>";
+          str += "<td class='col-md-8'>"+article_no+ " (" + size_code +")<input type='hidden' name='product_id[]' value='"+id+"' /></td>"; 
+          str += "<td class='col-md-1 text-center'><span id='Qty_"+id+"'>1</span><input id='TotalQty_"+id+"' type='hidden' name='product_qty[]' value='1' /></td>"; 
+          str += "<td class='col-md-1 text-center'><input id='ProductPrice_"+id+"' type='hidden' name='shoes_price[]' value='"+shoes_price+"' /><span id='TotalProductPrice_"+id+"'>"+shoes_price+"</span></td>"; 
+          str += "</tr>";
+          $('#ShowSaleProduct').prepend(str); 
+          }
+          
+        }
+        function DeleteProduct(id,shoes_price)
+        {
+        var ProductPrice = $("#ProductPrice_"+id+"").val(); 
+        var CurrentValue = $("#NetAmount").val();
+        var NetAmount = parseInt(CurrentValue) - parseInt(ProductPrice);
+        $("#NetAmount").val(NetAmount);  
+        $("#Product_"+id).remove();
+        }
+        
+        function RemoveRougeChar(convertString)
+        {
+          if(convertString.substring(0,1) == ",")
+          {
+            return convertString.substring(1, convertString.length)            
+          }
+          return convertString;
+        }
+        function GetAmount()
+        {
+          var PaidAmount = $("#PaidAmount").val();
+          $("#txtPaidAmount").html(PaidAmount);
+          PaidAmount = PaidAmount.replace(',', '');
+          var NetAmount = $("#NetAmount").val();
+            $("#txtNetAmount").html(NetAmount);
+          var DiscountAmount = $("#DiscountAmount").val();
+          $("#txtDiscountAmount").html(DiscountAmount);
+            var TotalAmount = parseInt(PaidAmount) - (parseInt(NetAmount) - parseInt(DiscountAmount));
+            TotalAmount = (isNaN(TotalAmount)) ? 0 : TotalAmount;
+              $("#ChangeAmount").val(TotalAmount);
+              $("#txtChangeAmount").html(TotalAmount);  
+        }
+
+    function CheckValidate()
+    {
+      $('#btnSave').prop('disabled', true);
+      var count_product = document.getElementsByClassName('count_product');
+      var PaidAmount = document.getElementById('PaidAmount').value;
+      if (count_product.length > 0) {
+        if(PaidAmount == 0)
+        {
+          alert('Please add paid amount!');
+          return false;   
+        }
+        else
+        {
+          var form = $('#FormID');
+          var NetAmount = $("#NetAmount").val();
+          var DiscountAmount = $("#DiscountAmount").val();
+          //var product_price = $("#DiscountAmount").val();
+          var product_id = $("input[name='product_id[]']").map(function(){return $(this).val();}).get();
+          var product_price = $("input[name='product_price[]']").map(function(){return $(this).val();}).get();
+          var product_qty = $("input[name='product_qty[]']").map(function(){return $(this).val();}).get();
+          $.ajax( {
+              type: "GET",
+              url : 'sale_product',
+              data: { 'net_amount':NetAmount, 'discount_amount': DiscountAmount, 'product_id': product_id, 'product_price': product_price, 'product_qty': product_qty},
+              success: function( response ) {
+                if(response == 'done')
+                {
+                  //printDiv();
+                  window.print();
+                  window.location.href = "/sale";
+                  return false;
+                }
+                //alert( response ); return false;
+              }
+            } );    
+          
+        }
+      }
+      else
+      {
+      alert('Please select any flavours!');
+      return false;
+      }
+    }
+
+    function searchKeyPress(e)
+    {
+        // look for window.event in case event isn't passed in
+        e = e || window.event;
+        if (e.keyCode == 13)
+        {
+            $('#btnSave').prop('disabled', true);
+            $('#PaidAmount').prop('disabled', true);
+            CheckValidate();
+            return false;
+        }
+        return true;
+    }
+        
 </script>
 <style type="text/css">
 .input_container1 {
